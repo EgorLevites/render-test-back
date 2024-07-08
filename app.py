75dfd8fe6c36d5552e284cc74f9e0337241f40e6
EgorLevites <egorfatnev95@gmail.com>
2024-07-08 14:42:59 +0300

    CORS(app)

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
