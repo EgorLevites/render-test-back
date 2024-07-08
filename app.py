a083a5bd74fc3a2eeb32fb37ee012f4d18977da1
EgorLevites <egorfatnev95@gmail.com>
2024-07-08 13:58:11 +0300

    db

--- app.py
+++ app.py
@@ -1,13 +1,56 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/data', methods=['GET'])
-def get_data():
-    return jsonify({"message": "Hello from Flask!"})
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
+    def __repr__(self):
+        return f'<User {self.name}>'
 
-if __name__ == "__main__":
+@app.route('/users', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    new_user = User(name=data['name'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User added successfully!"}), 201
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users_list = [{"id": user.id, "name": user.name, "email": user.email} for user in users]
+    return jsonify(users_list)
+
+@app.route('/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    data = request.get_json()
+    user = User.query.get(id)
+    if not user:
+        return jsonify({"message": "User not found!"}), 404
+
+    user.name = data.get('name', user.name)
+    user.email = data.get('email', user.email)
+    db.session.commit()
+    return jsonify({"message": "User updated successfully!"})
+
+@app.route('/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    if not user:
+        return jsonify({"message": "User not found!"}), 404
+
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": "User deleted successfully!"})
+
+if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
