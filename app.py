4cf39716abc85887545c92181f5452f08ee90cb4
EgorLevites <egorfatnev95@gmail.com>
2024-07-08 14:13:19 +0300

    with app.app_context()

--- app.py
+++ app.py
@@ -52,5 +52,6 @@ def delete_user(id):
     return jsonify({"message": "User deleted successfully!"})
 
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
